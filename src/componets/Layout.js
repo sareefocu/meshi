a83b7dbe58d9729ff72fddaa72aa10075c2c04c3
sareefocu <158563957+sareefocu@users.noreply.github.com>
2024-02-14 02:30:07 +0530

    Update Layout.js

--- src/componets/Layout.js
+++ src/componets/Layout.js
@@ -9,7 +9,23 @@ const Layout = ({ children, data }) => {
     useEffect(() => {
         if (data?.FacebookPixel) {
             // Extract the content within <script> tags
-            const match = data.FacebookPixel.match(/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi);
+            const match = `<!-- Meta Pixel Code -->
+            <script>
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '943598296691992');
+            fbq('track', 'PageView');
+            </script>
+            <noscript><img height="1" width="1" style="display:none"
+            src="https://www.facebook.com/tr?id=943598296691992&ev=PageView&noscript=1"
+            /></noscript>
+            <!-- End Meta Pixel Code -->`.match(/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi);
             const scriptContent = match ? match.join('') : '';
 
             // Remove <script> tags from the scriptContent
@@ -18,7 +34,23 @@ const Layout = ({ children, data }) => {
             setFacebookPixelScript(cleanedScript);
 
             // Extract the content within <noscript> tags
-            const noscriptMatch = data.FacebookPixel.match(/<noscript\b[^<]*(?:(?!<\/noscript>)<[^<]*)*<\/noscript>/gi);
+            const noscriptMatch = `<!-- Meta Pixel Code -->
+            <script>
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '943598296691992');
+            fbq('track', 'PageView');
+            </script>
+            <noscript><img height="1" width="1" style="display:none"
+            src="https://www.facebook.com/tr?id=943598296691992&ev=PageView&noscript=1"
+            /></noscript>
+            <!-- End Meta Pixel Code -->`.match(/<noscript\b[^<]*(?:(?!<\/noscript>)<[^<]*)*<\/noscript>/gi);
             const noscriptContent = noscriptMatch ? noscriptMatch.join('') : '';
 
             setFacebookPixelNoscript(noscriptContent);
